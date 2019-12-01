36f6eab4e49f427c25d1e611b78d6202be48ec4e
Nagarjun <deepaknagarjun@gmail.com>
2019-12-01 19:26:31 +0530

    commit on dec 1

--- server.js
+++ server.js
@@ -26,6 +26,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Routes
 app.use("/api/users", users);
 app.use("/api/matches", matches);
+
+// serving as static files
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // Listen
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening to ${port}`));
